c9dc977fa31fd5b1de3bcbaeb2c0de2960d15e07
zhangyimin <mrzhangym@126.com>
2025-08-03 22:39:47 +0800

    init project

---
+++ backend/src/main/java/com/dataconnai/DataConnAiApplication.java
@@ -0,0 +1,13 @@
+package com.dataconnai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataConnAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataConnAiApplication.class, args);
+    }
+
+}
