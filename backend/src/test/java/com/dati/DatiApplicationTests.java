19f4bf289b7baa3c35933c15d1f885e8d416855e
zhangyimin <mrzhangym@126.com>
2026-06-21 13:31:58 +0800

    修正用例

--- backend/src/test/java/com/dati/DatiApplicationTests.java
+++ backend/src/test/java/com/dati/DatiApplicationTests.java
@@ -1,10 +1,17 @@
 package com.dati;
 
+import com.dati.semantic.repository.dao.SemanticSearchDAO;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class DatiApplicationTests {
+
+    @MockitoBean
+    private SemanticSearchDAO semanticSearchDAO;
 
     @Test
     void contextLoads() {
