994dd8c6f2d02583e92e14be170162c574eab171
zhangyimin <mrzhangym@126.com>
2026-09-12 21:31:53 +0800

    0.6.2: add ecs deploy action

--- backend/core/src/main/java/com/dati/config/SpringDocConfig.java
+++ backend/core/src/main/java/com/dati/config/SpringDocConfig.java
@@ -21,7 +21,7 @@ public class SpringDocConfig {
         return new OpenAPI()
                 .info(new Info()
                         .title("DatI API")
-                        .version("0.6.0"));
+                        .version("0.6.2"));
     }
 
     /**
